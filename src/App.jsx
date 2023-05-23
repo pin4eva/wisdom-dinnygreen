4f7a3dd16b1bf176003da999d597712f11a7718b
Peter Akaliro <pin4eva@hotmail.com>
2023-05-23 14:01:40 +0100

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./styles/index.scss";
+
+const App = () => {
+  return (
+    <div className="home">
+      <div className="app">
+        <div className="app-box">Item 1</div>
+        <div className="app-box">Item 2</div>
+        <div className="app-box">Item 3</div>
+        <div className="app-box">Item 4</div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
